e11a0df4d676826e632de1517c12b1c4cd49aad1
velicanud <97797799+velicanud@users.noreply.github.com>
2023-02-03 09:16:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import numpy as np
+import streamlit as st
+
 col1, col2 = st.columns([1, 1])
 
 with col1:
@@ -8,4 +11,4 @@ with col1:
 with col2:
     number = st.number_input("Permutation size", step=1, value=1, min_value=1)
     if st.button("Permute"):
-	st.write(" ".join([str(n + 1) for n in np.random.permutation(number)]))
+        st.write(" ".join([str(n + 1) for n in np.random.permutation(number)]))
